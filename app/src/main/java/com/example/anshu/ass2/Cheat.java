e82ebf007896f6619bc7a17d2f893939546e8d33
nickeykumar <nickey14066@iiitd.ac.in>
2016-08-26 23:38:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anshu/ass2/Cheat.java
@@ -0,0 +1,15 @@
+package com.example.anshu.ass2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ANSHU on 8/26/2016.
+ */
+public class Cheat extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cheat);
+    }
+}
---
+++ app/src/main/java/com/example/anshu/ass2/Hint.java
@@ -0,0 +1,15 @@
+package com.example.anshu.ass2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ANSHU on 8/26/2016.
+ */
+public class Hint extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hint);
+    }
+}
